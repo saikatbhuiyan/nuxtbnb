95927f8f59247a7c8845620e20537c4c4465c9c1
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-10-25 01:30:20 +0600

    map in home details page

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -13,6 +13,8 @@
     <img src="/images/marker.svg" width="20" height="20">{{ home.location.address }} {{ home.location.city }} {{ home.location.state}}<br />
     <img src="/images/star.svg" width="20" height="20">   {{ home.reviewValue }} <br/>
     {{ home.guests }} guests, {{ home.pricePerNight }} /  {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath<br />
+    {{ home.description }}
+    <div style="height:800px;width:800px" ref="map"></div>
   </div>
 </template>
 
@@ -29,6 +31,9 @@ export default {
         return {
         home: {},
         };
+    },
+    mounted() {
+      this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng)
     },
     created() {
         const home = homes.find((home) => home.objectID == this.$route.params.id);

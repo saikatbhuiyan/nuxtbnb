1a7a7f89ddb5a359362d278581f2cbe068c7e1ae
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-10-27 01:16:29 +0600

    way to error handle

--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -1,19 +1,44 @@
-export default  function(context, inject) {
-    const appId = '7OVHMCS6BJ'
-    const apiKey = '364d0203fb2bf42e8f496f6654964a14'
+export default function(context, inject) {
+  const appId = "7OVHMCS6BJ";
+  const apiKey = "364d0203fb2bf42e8f496f6654964a14";
+  const headers = {
+    "X-Algolia-API-Key": apiKey,
+    "X-Algolia-Application-Id": appId,
+  };
+  inject("dataApi", {
+    getHome,
+  });
 
-    inject('dataApi', {
-        getHome
-    })
-
-    async function getHome(homeId) {
-        const response = await fetch(`https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`, {
-            headers: {
-                'X-Algolia-API-Key': apiKey,
-                'X-Algolia-Application-Id': appId,
-            }
-        })
-        const json = await response.json()
-        return json
+  async function getHome(homeId){
+    try{
+        return unWrap(
+          await fetch(
+            `https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`,
+            { headers }
+          )
+        );
+    } catch(error){
+        return getErrorResponse(error)
     }
+  } 
+
+  async function unWrap(response) {
+    const json = await response.json();
+    const { ok, status, statusText } = response;
+    return {
+      json,
+      ok,
+      status,
+      statusText,
+    };
+  }
+
+  function getErrorResponse(error){
+      return {
+          ok: false,
+          status: 500,
+          statusText: error.message,
+          json: {}
+      }
+  }
 }

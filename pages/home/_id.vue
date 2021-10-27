2bb9ef435a2bc3a33487c10cf3d07ef039ccfaa8
saikat <shaikotbhuiya@gmail.com>
2021-10-28 02:50:37 +0600

    upload review data in algolia and get by homeId

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -10,31 +10,60 @@
       />
     </div>
     {{ home.title }} <br />
-    <img src="/images/marker.svg" width="20" height="20">{{ home.location.address }} {{ home.location.city }} {{ home.location.state}}<br />
-    <img src="/images/star.svg" width="20" height="20">   {{ home.reviewValue }} <br/>
-    {{ home.guests }} guests, {{ home.pricePerNight }} /  {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath<br />
+    <img src="/images/marker.svg" width="20" height="20" />{{
+      home.location.address
+    }}
+    {{ home.location.city }} {{ home.location.state }}<br />
+    <img src="/images/star.svg" width="20" height="20" />
+    {{ home.reviewValue }} <br />
+    {{ home.guests }} guests, {{ home.pricePerNight }} /
+    {{ home.bedrooms }} rooms, {{ home.beds }} beds,
+    {{ home.bathrooms }} bath<br />
     {{ home.description }}
-    <div style="height:800px;width:800px" ref="map"></div>
+    <div style="height: 800px; width: 800px" ref="map"></div>
+    <div v-for="review in reviews" :key="review.objectId">
+        <img :src="review.reviewer.image" /><br/>
+        {{ review.reviewer.name }}<br/>
+        {{ review.date }}<br/>
+        {{ review.comment }}<br/>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    head() {
-        return {
-            title: this.home.title
-        }
-    },
-    mounted() {
-      this.$maps.showMap(this.$refs.map, this.home._geoloc.lat, this.home._geoloc.lng)
-    },
-    // `asyncData` is Nuxt feature for async Data, it's come with so many things
-    async asyncData({ params, $dataApi, error }) {
-      const response = await $dataApi.getHome(params.id)
-      if(!response.ok) return error({ statusCode: response.status, message: response.statusText });
-      return {
-        home: response.json,
-      }
-    },
+  head() {
+    return {
+      title: this.home.title,
+    };
+  },
+  mounted() {
+    this.$maps.showMap(
+      this.$refs.map,
+      this.home._geoloc.lat,
+      this.home._geoloc.lng
+    );
+  },
+  // `asyncData` is Nuxt feature for async Data, it's come with so many things
+  async asyncData({ params, $dataApi, error }) {
+    const homeResponse = await $dataApi.getHome(params.id);
+    if (!homeResponse.ok)
+      return error({
+        statusCode: homeResponse.status,
+        message: homeResponse.statusText,
+      });
+
+    const reviewResponse = await $dataApi.getReviewsByHomeId(params.id);
+    if (!reviewResponse.ok)
+      return error({
+        statusCode: reviewResponse.status,
+        message: reviewResponse.statusText,
+      });
+
+    return {
+      home: homeResponse.json,
+      reviews: reviewResponse.json.hits,
+    };
+  },
 };
 </script>
--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -7,20 +7,40 @@ export default function(context, inject) {
   };
   inject("dataApi", {
     getHome,
+    getReviewsByHomeId,
   });
 
-  async function getHome(homeId){
-    try{
-        return unWrap(
-          await fetch(
-            `https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`,
-            { headers }
-          )
-        );
-    } catch(error){
-        return getErrorResponse(error)
+  async function getHome(homeId) {
+    try {
+      return unWrap(
+        await fetch(
+          `https://${appId}-dsn.algolia.net/1/indexes/homes/${homeId}`,
+          { headers }
+        )
+      );
+    } catch (error) {
+      return getErrorResponse(error);
     }
-  } 
+  }
+
+  async function getReviewsByHomeId(homeId) {
+    try {
+      return unWrap(
+        await fetch(
+          `https://${appId}-dsn.algolia.net/1/indexes/reviews/query`,
+          {
+            headers,
+            method: "POST",
+            body: JSON.stringify({
+              filters: `homeId:${homeId}`,
+            }),
+          }
+        )
+      );
+    } catch (error) {
+      return getErrorResponse(error);
+    }
+  }
 
   async function unWrap(response) {
     const json = await response.json();
@@ -33,12 +53,12 @@ export default function(context, inject) {
     };
   }
 
-  function getErrorResponse(error){
-      return {
-          ok: false,
-          status: 500,
-          statusText: error.message,
-          json: {}
-      }
+  function getErrorResponse(error) {
+    return {
+      ok: false,
+      status: 500,
+      statusText: error.message,
+      json: {},
+    };
   }
 }

fdc23015a70f73bd1a024bc075f1bec741be5c71
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-11-01 22:30:26 +0600

    refactor for run at a time more request

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -22,21 +22,21 @@
     {{ home.description }}
     <div style="height: 800px; width: 800px" ref="map"></div>
     <div v-for="review in reviews" :key="review.objectId">
-        <img :src="review.reviewer.image" /><br/>
-        {{ review.reviewer.name }}<br/>
-        {{ formatDate(review.date) }}<br/>
-        <short-text :text="review.comment" :target="120" /><br/>
+      <img :src="review.reviewer.image" /><br />
+      {{ review.reviewer.name }}<br />
+      {{ formatDate(review.date) }}<br />
+      <short-text :text="review.comment" :target="120" /><br />
     </div>
-    <img :src="user.image"><br/>
-    {{ user.name }} <br/>
-    {{ formatDate(user.joined) }} <br/>
-    {{ user.reviewCount }} <br/>
-    {{ user.description }} <br/>
+    <img :src="user.image" /><br />
+    {{ user.name }} <br />
+    {{ formatDate(user.joined) }} <br />
+    {{ user.reviewCount }} <br />
+    {{ user.description }} <br />
   </div>
 </template>
 
 <script>
-import ShortText from '../../components/ShortText.vue';
+import ShortText from "../../components/ShortText.vue";
 export default {
   components: { ShortText },
   head() {
@@ -53,38 +53,66 @@ export default {
   },
   // `asyncData` is Nuxt feature for async Data, it's come with so many things
   async asyncData({ params, $dataApi, error }) {
-    const homeResponse = await $dataApi.getHome(params.id);
-    if (!homeResponse.ok)
+    const responses = await Promise.all([
+      $dataApi.getHome(params.id),
+      $dataApi.getReviewsByHomeId(params.id),
+      $dataApi.getUserByHomeId(params.id),
+    ]);
+
+    const badResponse = responses.find((response) => !response.ok);
+    if (badResponse)
       return error({
-        statusCode: homeResponse.status,
-        message: homeResponse.statusText,
+        statusCode: badResponse.status,
+        message: badResponse.statusText,
       });
 
-    const reviewResponse = await $dataApi.getReviewsByHomeId(params.id);
-    if (!reviewResponse.ok)
-      return error({
-        statusCode: reviewResponse.status,
-        message: reviewResponse.statusText,
-      });
+      return {
+        home: responses[0].json,
+        reviews: responses[1].json.hits,
+        user: responses[2].json.hits[0],
+      };
 
-    const userResponse = await $dataApi.getUserByHomeId(params.id);
-    if (!userResponse.ok)
-      return error({
-        statusCode: userResponse.status,
-        message: userResponse.statusText,
-      });
+    // const homeResponse = await $dataApi.getHome(params.id);
+    // if (!homeResponse.ok)
+    //   return error({
+    //     statusCode: homeResponse.status,
+    //     message: homeResponse.statusText,
+    //   });
+    // if (!homeResponse.ok)
+    //   return error({
+    //     statusCode: homeResponse.status,
+    //     message: homeResponse.statusText,
+    //   });
 
-    return {
-      home: homeResponse.json,
-      reviews: reviewResponse.json.hits,
-      user: userResponse.json.hits[0],
-    };
+    // const reviewResponse = await $dataApi.getReviewsByHomeId(params.id);
+    // if (!reviewResponse.ok)
+    //   return error({
+    //     statusCode: reviewResponse.status,
+    //     message: reviewResponse.statusText,
+    //   });
+
+    // const userResponse = await $dataApi.getUserByHomeId(params.id);
+    // if (!userResponse.ok)
+    //   return error({
+    //     statusCode: userResponse.status,
+    //     message: userResponse.statusText,
+    //   });
+
+    // return {
+    //   home: homeResponse.json,
+    //   reviews: reviewResponse.json.hits,
+    //   user: userResponse.json.hits[0],
+    // };
   },
-  methods:{
-      formatDate(dateStr){
-          const date = new Date(dateStr)
-          return date.toLocaleDateString(undefined, {month: 'long', year: 'numeric', day: '2-digit'})
-      }
-  }
+  methods: {
+    formatDate(dateStr) {
+      const date = new Date(dateStr);
+      return date.toLocaleDateString(undefined, {
+        month: "long",
+        year: "numeric",
+        day: "2-digit",
+      });
+    },
+  },
 };
 </script>

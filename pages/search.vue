14d0a9d0061f14c9ba05f3899a4a6883b541b463
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-11-06 13:16:30 +0600

    add marker in showMap

--- pages/search.vue
+++ pages/search.vue
@@ -3,7 +3,9 @@
         <div>Result for: {{ label }}</div>
         <div style="height: 800px; width: 800px;float:right;" ref="map"></div>
         <div v-if="homes.length > 0">
-            <HomeRow v-for="home in homes" :key="home.objectID" :home="home"/>
+            <nuxt-link v-for="home in homes" :key="home.objectID" :to="`/home/${home.objectID}`">
+                <HomeRow :home="home"/>
+            </nuxt-link>
         </div>
         <div v-else>No results found</div>
     </div>
@@ -19,7 +21,14 @@ export default {
     },
     methods: {
         updateMap() {
-            this.$maps.showMap(this.$refs.map, this.lat, this.lng);
+            this.$maps.showMap(this.$refs.map, this.lat, this.lng, this.getHomeMarkers());
+        },
+        getHomeMarkers(){
+            return this.homes.map((home) => {
+                return {
+                    ...home._geoloc,
+                }
+            })
         }
     },
     // watchQuery: ['lat'], //watchQuery is nuxt feature refresh data when 'lat' change

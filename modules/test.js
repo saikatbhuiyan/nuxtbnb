8edfffd20c3813549e2e16ddb4b05f6343fc9e16
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-11-08 09:12:28 +0600

    add tailwindcss

--- modules/test.js
+++
@@ -1,8 +0,0 @@
-export default function(){
-//     this.nuxt.hook('build:before', () => {
-//         console.log('build before!!!')
-//     })
-    this.nuxt.hook('render:route', (url, result, context) => {
-        context.res.setHeader('X-Powered-By', 'The Force!')
-    })
-}

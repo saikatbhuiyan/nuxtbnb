be8320ed248a0d30e59edfeb2bd0a07daa8f5046
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-11-08 08:39:13 +0600

    basic example of hook

--- modules/test.js
+++ modules/test.js
@@ -1,3 +1,8 @@
 export default function(){
-
+//     this.nuxt.hook('build:before', () => {
+//         console.log('build before!!!')
+//     })
+    this.nuxt.hook('render:route', (url, result, context) => {
+        context.res.setHeader('X-Powered-By', 'The Force!')
+    })
 }

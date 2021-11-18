69852a6851309ccb70d4a01fe39b83ec0c53ef37
saikatbhuiyan <shaikotbhuiya@gmail.com>
2021-11-18 15:53:30 +0600

    header design

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="app">
+    <header class="app-header">
+      <div class="app-logo">
+        <img src="/images/logo.svg" />
+      </div>
+      <div class="app-search">
+        <input
+          type="text"
+          ref="citySearch"
+          @changed="changed"
+          placeholder="Enter your address"
+        />
+        <input type="text" class="datepicker" placeholder="Check in" />
+        <input type="text" class="datepicker" placeholder="Check out" />
+        <button>
+          <img src="/images/icons/search.svg" />
+        </button>
+      </div>
+      <div class="app-user-menu">
+        <img src="/images/icons/house.svg" />
+        <div class="name">Host</div>
+        <img src="/images/user.jpg" class="avatar" />
+      </div>
+    </header>
     <header style="background-color: #fff">
       <nuxt-link to="/">Home</nuxt-link>
       <input type="text" ref="citySearch" @changed="changed" />
